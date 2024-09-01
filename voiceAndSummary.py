4009ad25524f93caa0f5d7eb922453492023a369
akhileshnair2024 <akhileshnair2024@u.northwestern.edu>
2024-09-01 15:23:05 -0500

    first commit

---
+++ voiceAndSummary.py
@@ -0,0 +1,23 @@
+import openai
+import os
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+# utilize whisper-1 to acquire the text from audio file
+with open("C:/Users/akhil/OneDrive/Documents/Creative Endeavour/OpenAI Transcriber/testAudio2.mp3", "rb") as audio_file:
+    transcription = openai.Audio.transcribe("whisper-1", audio_file)
+
+# print text for user to see
+print(transcription['text'])
+
+# utilize gpt 4o mini so i can see the response 
+response = openai.ChatCompletion.create(
+  model="gpt-4o-mini",
+  messages=[
+    {"role": "system", "content": "You will summarize the information that you receive in a meaningful and useful manner, to be utilized as meeting notes."},
+    {"role": "user", "content": transcription['text']}
+  ]
+)
+
+message_content = response['choices'][0]['message']['content']
+print("Summary:", message_content)
